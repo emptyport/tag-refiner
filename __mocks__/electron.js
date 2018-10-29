e8108704db428976837bf6b8a6c0cae38b5d1fbb
Michael Porter <michaelporter.chemistry@gmail.com>
2018-10-29 13:54:03 -0500

    created mock function for electron

---
+++ __mocks__/electron.js
@@ -0,0 +1,5 @@
+export const remote = {
+  dialog: {
+    showOpenDialog: jest.fn().mockReturnValue('path/to/output folder')
+  }
+};
--- jest.config.js
+++ jest.config.js
@@ -74,7 +74,8 @@ module.exports = {
   // moduleNameMapper: {},
   "moduleNameMapper": {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+    "electron": "<rootDir>/__mocks__/electron.js"
   },
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
